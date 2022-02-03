2b5ce461bb5b0f3a4d5900895a531e26ce3d0ad0
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2022-02-03 16:31:20 +0530

    Update modules.py

--- TGRobot/modules/modules.py
+++ TGRobot/modules/modules.py
@@ -18,13 +18,13 @@ from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
 
-@run_async
+
 @dev_plus
 def load(update: Update, context: CallbackContext):
     message = update.effective_message
     text = message.text.split(" ", 1)[1]
     load_messasge = message.reply_text(
-        f"Attempting to load module : <b>{text}</b>", parse_mode=ParseMode.HTML
+        f"Attempting to load module : <b>{text}</b>", parse_mode=ParseMode.HTML,
     )
 
     try:
@@ -46,13 +46,12 @@ def load(update: Update, context: CallbackContext):
         for handler in handlers:
             if not isinstance(handler, tuple):
                 dispatcher.add_handler(handler)
+            elif isinstance(handler[0], collections.Callable):
+                callback, telethon_event = handler
+                telethn.add_event_handler(callback, telethon_event)
             else:
-                if isinstance(handler[0], collections.Callable):
-                    callback, telethon_event = handler
-                    telethn.add_event_handler(callback, telethon_event)
-                else:
-                    handler_name, priority = handler
-                    dispatcher.add_handler(handler_name, priority)
+                handler_name, priority = handler
+                dispatcher.add_handler(handler_name, priority)
     else:
         IMPORTED.pop(imported_module.__mod_name__.lower())
         load_messasge.edit_text("The module cannot be loaded.")
@@ -84,21 +83,21 @@ def load(update: Update, context: CallbackContext):
         USER_SETTINGS[imported_module.__mod_name__.lower()] = imported_module
 
     load_messasge.edit_text(
-        "Successfully loaded module : <b>{}</b>".format(text), parse_mode=ParseMode.HTML
+        "Successfully loaded module : <b>{}</b>".format(text), parse_mode=ParseMode.HTML,
     )
 
 
-@run_async
+
 @dev_plus
 def unload(update: Update, context: CallbackContext):
     message = update.effective_message
     text = message.text.split(" ", 1)[1]
     unload_messasge = message.reply_text(
-        f"Attempting to unload module : <b>{text}</b>", parse_mode=ParseMode.HTML
+        f"Attempting to unload module : <b>{text}</b>", parse_mode=ParseMode.HTML,
     )
 
     try:
-        imported_module = importlib.import_module("YoneRobot.modules." + text)
+        imported_module = importlib.import_module("TGRobot.modules." + text)
     except:
         unload_messasge.edit_text("Does that module even exist?")
         return
@@ -116,15 +115,14 @@ def unload(update: Update, context: CallbackContext):
             if isinstance(handler, bool):
                 unload_messasge.edit_text("This module can't be unloaded!")
                 return
-            elif not isinstance(handler, tuple):
+            if not isinstance(handler, tuple):
                 dispatcher.remove_handler(handler)
+            elif isinstance(handler[0], collections.Callable):
+                callback, telethon_event = handler
+                telethn.remove_event_handler(callback, telethon_event)
             else:
-                if isinstance(handler[0], collections.Callable):
-                    callback, telethon_event = handler
-                    telethn.remove_event_handler(callback, telethon_event)
-                else:
-                    handler_name, priority = handler
-                    dispatcher.remove_handler(handler_name, priority)
+                handler_name, priority = handler
+                dispatcher.remove_handler(handler_name, priority)
     else:
         unload_messasge.edit_text("The module cannot be unloaded.")
         return
@@ -155,11 +153,11 @@ def unload(update: Update, context: CallbackContext):
         USER_SETTINGS.pop(imported_module.__mod_name__.lower())
 
     unload_messasge.edit_text(
-        f"Successfully unloaded module : <b>{text}</b>", parse_mode=ParseMode.HTML
+        f"Successfully unloaded module : <b>{text}</b>", parse_mode=ParseMode.HTML,
     )
 
 
-@run_async
+
 @sudo_plus
 def listmodules(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -168,16 +166,16 @@ def listmodules(update: Update, context: CallbackContext):
     for helpable_module in HELPABLE:
         helpable_module_info = IMPORTED[helpable_module]
         file_info = IMPORTED[helpable_module_info.__mod_name__.lower()]
-        file_name = file_info.__name__.rsplit("YoneRobot.modules.", 1)[1]
+        file_name = file_info.__name__.rsplit("TGRobot.modules.", 1)[1]
         mod_name = file_info.__mod_name__
         module_list.append(f"- <code>{mod_name} ({file_name})</code>\n")
     module_list = "Following modules are loaded : \n\n" + "".join(module_list)
     message.reply_text(module_list, parse_mode=ParseMode.HTML)
 
 
-LOAD_HANDLER = CommandHandler("load", load)
-UNLOAD_HANDLER = CommandHandler("unload", unload)
-LISTMODULES_HANDLER = CommandHandler("listmodules", listmodules)
+LOAD_HANDLER = CommandHandler("load", load, run_async=True)
+UNLOAD_HANDLER = CommandHandler("unload", unload, run_async=True)
+LISTMODULES_HANDLER = CommandHandler("listmodules", listmodules, run_async=True)
 
 dispatcher.add_handler(LOAD_HANDLER)
 dispatcher.add_handler(UNLOAD_HANDLER)
